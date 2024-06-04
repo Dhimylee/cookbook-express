8506acf5716e61a0c1e6a7115cbecc0c2b39bd21
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2024-06-03 23:38:50 -0300

    Feat: pagina de perfil

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,9 +1,90 @@
+<style>
+    * {
+        list-style: none;
+    }
+    main {
+        background-color: #FBF7ED;
+        height: 95%;
+    }
+    .perfil__header h1 {
+        color: #FF9E0B;
+        font-size: 36px;
+        font-weight: 500;
+        letter-spacing: normal;
+        line-height: 120%;
+        padding: 20px 0px 0px 50px;
+    }
+    .perfil__wrapper {
+        display: flex;
+        overflow-x: hidden;
+        width: 100%;
+    }
+    .perfil__container {
+        background-color: #fff;
+        border: 1px solid #FBF7ED;
+        border-radius: 12px;
+        display: flex;
+        justify-content: space-between;
+        margin: 0 auto;
+        padding: 30px 20px;
+        width: 100%;
+    }
+    .perfil__col {
+        border-right: 1px solid rgba(234, 195, 157, .5);
+        margin-right: 20px;
+        min-width: 212px;
+        width: 20vw;
+    }
+    .perfil__iten span {
+        background-color: #FBF7ED;
+        border: 1px solid #FF9E0B;
+        border-radius: 8px;
+        color: #FF9E0B;
+        font-size: 14px;
+        padding: 8px 16px;
+    }
+    .perfil__dados {
+        padding: 8px 20px;
+        width: 100%;
+    }
+</style>
+
 @extends('components.base')
 
 @section('title', 'Perfil')
 
 @section('content')
-    <h1>Perfil</h1>
+    <div>
+        <div class="perfil__header">
+            <h1>Meu perfil</h1>
+        </div>
+
+        <div class="perfil__wrapper">
+            <div class="perfil__container">
+                <aside class="perfil__col">
+                    <nav>
+                        <ul class="perfil__list">
+                            <li class="perfil__iten"><span>dados pessoais</span></li>
+                        </ul>
+                    </nav>
+                </aside>
+                <section class="perfil__dados">
+                    <div>
+                        <h2>dados</h2>
+                        <div class="perfil__dados-info">
+                            <form action="">
+                                <div>
+                                    <label>nome</label>
+                                    <p>{{ $user->name }}</p>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <!-- <h1 class="perfil__title">Perfil</h1>
 
     <p>Nome: {{ $user->name }}</p>
     <p>E-mail: {{ $user->email }}</p>
@@ -19,7 +100,7 @@
             @csrf
             <button type="submit">Logout</button>
         </form>
-    @endif
+    @endif -->
 @endsection
 
 @section('style')
