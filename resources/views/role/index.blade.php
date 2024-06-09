cb405497e8f06801eb869bf8ba9e4d73794b250c
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2024-06-09 12:21:32 -0300

    Ajustes visuais na tabela de cargos

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -31,6 +31,29 @@
         padding: 10px;
         border-bottom: 1px solid #FF9E0B;
     }
+    .box-cargos__acoes {
+        display: flex;
+        gap: 20px;
+    }
+    /* .form-delet {
+        padding: 10px;
+        font-size: 20px;
+    } */
+    .ativo {
+        background-color: #7ed27e;
+        color: #fff;
+        border-radius: 10px;
+        text-align: center;
+        width: 80%;
+    }
+
+    .inativo {
+        background-color: red;
+        color: #fff;
+        border-radius: 10px;
+        text-align: center;
+        width: 80%;
+    }
 </style>
 @extends('components.base')
 
@@ -44,34 +67,48 @@
             @if(session('error'))
                 <p>{{session('error')}}</p>
             @endif
+            <table>
+                <thead>
+                    <tr>
+                        <th>Cargo</th>
+                        <th>Status</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                @foreach ($roles as $role)
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>{{ $role->name }}</p>
+                            </td>
+                            <td>
+                                <p class="{{ $role->active ? 'ativo' : 'inativo' }}">{{$role->active ? 'Ativo' : 'Inativo'}}</p>
+                            </td>
+                            <td class="box-cargos__acoes">
+                                @if($role->active)
+                                    <form action="{{route('role.deactivate')}}" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="id" value="{{$role->id}}">
+                                        <button type="submit">Desativar</button>
+                                    </form>
+                                @else
+                                    <form action="{{route('role.activate')}}" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="id" value="{{$role->id}}">
+                                        <button type="submit">Ativar</button>
+                                    </form>
+                                @endif
 
-            @foreach ($roles as $role)
-                <div class="d-flex flex-row">
-                    <h3>{{ $role->name }}</h3>
-                    <p>Status: {{$role->active ? 'Ativo' : 'Inativo'}}</p>
-
-                    @if($role->active)
-                        <form action="{{route('role.deactivate')}}" method="POST">
-                            @csrf
-                            <input type="hidden" name="id" value="{{$role->id}}">
-                            <button type="submit">Desativar</button>
-                        </form>
-                    @else
-                        <form action="{{route('role.activate')}}" method="POST">
-                            @csrf
-                            <input type="hidden" name="id" value="{{$role->id}}">
-                            <button type="submit">Ativar</button>
-                        </form>
-                    @endif
-
-                    <form action="{{route('role.delete')}}" method="POST">
-                        @csrf
-                        <input type="hidden" name="id" value="{{$role->id}}">
-                        <button type="submit">Deletar</button>
-                    </form>
-
-                </div>
-            @endforeach
+                                <form action="{{route('role.delete')}}" method="POST" class="form-delet">
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{$role->id}}">
+                                    <button type="submit">Deletar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    </tbody>            
+                @endforeach
+            </table>
         </div>
     </section>
     <!-- <h1>Cargos</h1>
