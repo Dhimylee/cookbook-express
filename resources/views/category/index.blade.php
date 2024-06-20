f88e0c4df4f8c73c9db7751094064df82b8c7de0
Marcelo Santos <marcelo.santos@poder360.com.br>
2024-06-20 00:16:50 -0300

    Hotfix: Pack de alterações em Category, Restaurant, Role e User

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,35 +1,46 @@
-
-
 @extends('components.base')
 
 @section('title', 'Home')
 
 @section('content')
-    <section class="box-categorias">
-        <div class="box-categoriasWrapper">
-            <h1>Categorias</h1>
+    <h1>Categorias</h1>
 
-            @if(session('error'))
-                <p>{{session('error')}}</p>
-            @endif
-            <table>
-                <thead>
+    @if(session('error'))
+        <p>{{session('error')}}</p>
+    @endif
+    <div class="formulario">
+    @foreach ($categories as $category)
+    
+        <div class="d-flex flex-row listagem">
+                <h3>{{ $category->name }}</h3>
 
-                    <form action="{{route('category.store')}}" method="POST">
+                <form action="{{route('category.update')}}" method="POST">
+                    @csrf
+                    <input type="hidden" name="id" value="{{$category->id}}">
+                    <input type="text" name="name" placeholder="Atualizar nome da categoria">
+                    <button type="submit" class="salvar">Atualizar</button>
+                </form>
+
+                <form action="{{route('category.delete')}}" method="POST">
+                    @csrf
+                    <input type="hidden" name="id" value="{{$category->id}}">
+                    <button type="submit" class="deletar">Deletar</button>
+                </form>
+
+            </div>
+    
+
+    @endforeach
+    </div>
+    <h1>Cadastrar categoria</h1>
+
+    <form action="{{route('category.store')}}" method="POST" class="formulario">
         @csrf
-        <label for="category">Criar Categoria: </label>
-        <input type="text" name="name" id="category" list="categories" placeholder="Adicione a nova categoria">
-        <form action="{{route('measure.store')}}" method="POST" class="formulario">
-
-
-        @csrf
+        <input type="text" name="name" placeholder="Nome da categoria">
         <button type="submit" class="salvar">Criar</button>
-</form>
+    </form>
 
-            </table>
-        </div>
-    </section>
-    @endsection
+@endsection
 
 @section('style')
 <style>
@@ -39,8 +50,8 @@
         border-radius: 8px;
         color: #FF9E0B;
         font-size: 14px;
-        padding: 3px 30px;
-        display: block;
+        padding: 12px 18px;
+
     }
     main {
         background-color: #FBF7ED;
@@ -73,6 +84,46 @@
         padding: 10px;
         border-bottom: 1px solid #FF9E0B;
     }
+    .formulario form > input{
+        background-color: #fff;
+        border-radius: 4px;
+        height: 48px;
+        transition: border .2s ease-in-out;
+        width: 200px;
+        color: rgba(0, 0, 0, .4);
+        font-weight: 400;
+        margin-bottom: 15px;
+        border: 1px solid rgba(234, 195, 157, .5);
+        font-size: 15px;
+    }
+    .formulario{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        padding: 20px;
+    }
+    .deletar{
+        background-color: #fbeded;
+        border: 1px solid #a2363b;
+        border-radius: 8px;
+        color: #e41313;
+        font-size: 14px;
+        padding: 12px 16px;
+    }
+    h1 {
+        color: #FF9E0B;
+        font-size: 36px;
+        font-weight: 500;
+        letter-spacing: normal;
+        line-height: 120%;
+        padding: 20px 0px 20px 50px;
+    }
+    h3 {
+        font-size: 1.3rem;
+    }
+    .listagem{
+        margin-bottom: 50px;
+    }
+
 </style>
 @endsection
 
